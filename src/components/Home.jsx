21ae8639093796d1f57d1398194d9d16bd92554d
Kamesh255 <kameshhedau19@gmail.com>
2022-07-12 19:52:54 +0530

    install tailwindcss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
             <div style={{marginLeft:'5%'}}>
                 <h2>Today's Schedule</h2>
             </div>
-            <div style={{marginRight:'5%', display:'flex',gap:'10px'}}> 
-                <button style={{padding:'7px', backgroundColor:'black',color:'#FFFFFF',fontWeight:'bold',borderRadius:'7px'}}>BOOKMARKS</button>
-                <button  style={{padding:'7px', backgroundColor:'black',color:'#FFFFFF',fontWeight:'bold',borderRadius:'7px'}}>REFER & EARN</button>
+            <div style={{marginRight:'5%', display:'flex',gap:'10px'}}>  
+                <button style={{padding:'7px', backgroundColor:'black',color:'#FFFFFF',fontWeight:'bold',borderRadius:'7px'}} >BOOKMARKS</button>
+                <button style={{padding:'7px', backgroundColor:'black',color:'#FFFFFF',fontWeight:'bold',borderRadius:'7px'}}>REFER & EARN</button>
             </div>
         </div>
     </div>
