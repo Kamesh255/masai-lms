c7039c22f27dd2f6e2fc5a02904f48dabd040b91
Kamesh255 <kameshhedau19@gmail.com>
2022-07-13 17:38:49 +0530

    update login

--- masai/src/components/Login.jsx
+++ masai/src/components/Login.jsx
@@ -1,28 +1,89 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div style={{height:'100vh'}}>
-        <div style={{margin:'100px auto',height:'400px', maxWidth:'500px',backgroundColor:'#FFFFFF', borderRadius:'5px', display:'grid',gridTemplateRows:'20% 80%' ,alignItems:'center'}}>
-            <div style={{margin:'auto'}}>
-            <img style={{width:'130px'}} src="masai_logo.jpg" alt="" />
-            </div> 
-            <div style={{display:'grid',gap:"10px",textAlign:'left',marginLeft:'10%',}}>
-                <p>Email</p>
-                <input style={{maxWidth:'350px',height:'40px', borderRadius:'5px'}} type="text" placeholder='Enter your email / username '  />
-               <p>Password</p>
-                <input style={{maxWidth:'350px',height:'40px', borderRadius:'5px'}} type='password' placeholder='Enter your password'/>
-                <p style={{marginLeft:'55px'}}>Forgat your password</p>
-                <button style={{width:'fit-content', backgroundColor:'black', color:'#ffffff', padding:'3px', borderRadius:'5px', fontWeight:'500',position:'relative',left:"60%",top:'-35px'}}>LOG IN</button>
-            </div>
+    <div style={{ height: "800px" }}>
+      <div
+        style={{
+          margin: "80px auto",
+          maxWidth: "500px",
+          backgroundColor: "#FFFFFF",
+          borderRadius: "5px",
+          display: "grid",
+          gridTemplateRows: "20% 80%",
+          alignItems: "center",
+          height: "700px",
+        }}
+      >
+        <div style={{ margin: "auto" }}>
+          <img style={{ width: "130px" }} src="masai_logo.jpg" alt="" />
         </div>
-        <div>
 
-        </div> 
+        <div
+          style={{
+            display: "grid",
+            gap: "10px",
+            textAlign: "left",
+            marginLeft: "10%",
+          }}
+        >
+          <h1 style={{ fontWeight: "bold" }}>Register User</h1>
+          <p>Email</p>
+          <input
+            style={{ maxWidth: "350px", height: "40px", borderRadius: "5px",backgroundColor:'#E8F0FE' }}
+            type="text"
+            placeholder="Enter your email / username "
+          />
+          <p>Password</p>
+          <input
+            style={{ maxWidth: "350px", height: "40px", borderRadius: "5px",backgroundColor:'#E8F0FE' }}
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button
+            style={{
+              width: "fit-content",
+              backgroundColor: "black",
+              color: "#ffffff",
+              padding: "3px",
+              borderRadius: "5px",
+              fontWeight: "500",
+            }}
+          >
+            Register
+          </button>
+          <br />
+          <hr />
+          <br />
+          <h1 style={{ fontWeight: "bold" }}>Log-in User</h1>
+          <p>Email</p>
+          <input
+            style={{ maxWidth: "350px", height: "40px", borderRadius: "5px",backgroundColor:'#E8F0FE' }}
+            type="text"
+            placeholder="Enter your email / username "
+          />
+          <p>Password</p>
+          <input
+            style={{ maxWidth: "350px", height: "40px", borderRadius: "5px",backgroundColor:'#E8F0FE' }}
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button
+            style={{
+              width: "fit-content",
+              backgroundColor: "black",
+              color: "#ffffff",
+              padding: "3px",
+              borderRadius: "5px",
+              fontWeight: "500",
+            }}
+          >
+            LOG IN
+          </button>
+        </div>  
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
-
- 
+export default Login;
