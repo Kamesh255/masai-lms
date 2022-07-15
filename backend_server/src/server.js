d3a31c63d6f57023f9721e4d330e636185a1afe9
Kamesh255 <kameshhedau19@gmail.com>
2022-07-15 20:20:47 +0530

    add get, post lecture

--- backend_server/src/server.js
+++ backend_server/src/server.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const connect = require("./configs/db")
 
+const lectureController = require("./controllers/lecture.controller");
+
 const app = express();
 
 app.use(express.json());  
+
+app.use("/lecture", lectureController);
 
 const port  =  3001
 app.listen(port, async function(){
