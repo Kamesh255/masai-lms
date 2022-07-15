fc086618a75f4c9b50284477bc95f26ebad4476e
Kamesh255 <kameshhedau19@gmail.com>
2022-07-15 20:53:46 +0530

    add get, post assignment

--- backend_server/src/server.js
+++ backend_server/src/server.js
@@ -4,11 +4,14 @@ const connect = require("./configs/db")
 
 const lectureController = require("./controllers/lecture.controller");
 
+const assignmentController = require("./controllers/assignment.controller"); 
+
 const app = express();
 
 app.use(express.json());  
 
 app.use("/lecture", lectureController);
+app.use("/assignment", assignmentController);
 
 const port  =  3001
 app.listen(port, async function(){
